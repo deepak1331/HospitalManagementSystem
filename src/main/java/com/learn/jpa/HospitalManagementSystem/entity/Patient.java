f70c3ea248b25014f10dba94f10b7b1b9eedb240
Deepak Yadav <deepakyadav1331@gmail.com>
2025-08-26 19:06:57 +0530

    Added @Builder

--- src/main/java/com/learn/jpa/HospitalManagementSystem/entity/Patient.java
+++ src/main/java/com/learn/jpa/HospitalManagementSystem/entity/Patient.java
@@ -1,12 +1,18 @@
 package com.learn.jpa.HospitalManagementSystem.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Patient {
 
     @Id
