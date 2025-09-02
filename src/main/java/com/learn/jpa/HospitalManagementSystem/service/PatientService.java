ac3b94f28d13a2f38fc9a6d5248605412960047a
Deepak Yadav <deepakyadav1331@gmail.com>
2025-09-02 14:35:38 +0530

    Added GenderService, with @transactional annotation

---
+++ src/main/java/com/learn/jpa/HospitalManagementSystem/service/PatientService.java
@@ -0,0 +1,24 @@
+package com.learn.jpa.HospitalManagementSystem.service;
+
+import com.learn.jpa.HospitalManagementSystem.entity.Patient;
+import com.learn.jpa.HospitalManagementSystem.repo.PatientRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PatientService {
+
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @Transactional
+    public Patient getPatientById(Long id) {
+        Patient p1 = patientRepository.findById(id).orElseThrow();
+        Patient p2 = patientRepository.findById(id).orElseThrow();
+        return p2;
+    }
+
+}
