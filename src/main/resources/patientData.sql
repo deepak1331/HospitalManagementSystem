bc7a1d6912cd45c633de40a13e1b2f692f932eff
Deepak Yadav <deepakyadav1331@gmail.com>
2025-09-02 16:13:42 +0530

    Added patientData.sql, and config to execute insert statement at application startup

---
+++ src/main/resources/patientData.sql
@@ -0,0 +1,6 @@
+INSERT INTO hms.patient_tbl (birth_date, gender, created_on, phone_no, email, patient_name, blood_group) VALUES
+('1988-06-13', 'Male', NOW(), '7401087950', 'deepakyadav1331@gmail.com', 'Deepak Yadav', 'AB+'),
+('1989-09-03', 'Female', NOW(), '7776688888', 'shiprayadav@gmail.com', 'Shipra Yadav', 'O-'),
+('2021-12-20', 'Male',NOW(), '7776688123', 'avyaanyadav@gmail.com', 'Avyaan Yadav', 'B+'),
+('1991-09-08', 'Male', NOW(), '7723432438', 'shubhamyadav123@gmail.com', 'Shubham Yadav', 'A+'),
+('1995-04-16', 'Female', NOW(), '7723432347', 'shikhayadav16@gmail.com', 'Shikha Yadav', 'A-');
