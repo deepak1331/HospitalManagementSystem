5c216a7a73fc7d59255ad59d523589d083bdea70
Deepak Yadav <deepakyadav1331@gmail.com>
2025-09-02 14:35:51 +0530

    Added PatientTest

--- src/test/java/com/learn/jpa/HospitalManagementSystem/entity/PatientTest.java
+++ src/test/java/com/learn/jpa/HospitalManagementSystem/entity/PatientTest.java
@@ -1,11 +1,11 @@
 package com.learn.jpa.HospitalManagementSystem.entity;
 
+import com.learn.jpa.HospitalManagementSystem.constant.Gender;
 import com.learn.jpa.HospitalManagementSystem.repo.PatientRepository;
+import com.learn.jpa.HospitalManagementSystem.service.PatientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.util.Assert;
 
 import java.time.LocalDate;
@@ -15,7 +15,10 @@ import java.util.List;
 public class PatientTest {
 
     @Autowired
-    private PatientRepository repository;
+    private PatientRepository patientRepository;
+
+    @Autowired
+    private PatientService patientService;
 
     @Test
     public void testAddPatient() {
@@ -25,11 +28,35 @@ public class PatientTest {
         Patient patient = Patient.builder()
                 .name("Deepak Yadav")
                 .birthDate(dob)
-                .gender("Male")
+                .gender(Gender.Male.toString())
                 .email("deepakyadav1331@gmail.com")
                 .phoneNo("7777788888").build();
 
-        Patient result = repository.save(patient);
+        Patient result = patientRepository.save(patient);
+        System.out.printf("Patient Saved: %s", result);
+        Assert.notNull(patient, "Not null");
+    }
+
+    @Test
+    public void testAddAllPatient() {
+        LocalDate dob = LocalDate.of(1989, 9 ,3);
+        //Patient patient = new Patient(1L, "Deepak Yadav", dob, "Male",
+        //                      "deepakyadav1331@gmail.com", "7777788888");
+        Patient patient = Patient.builder()
+                .name("Shipra Yadav")
+                .birthDate(dob)
+                .gender(Gender.Female.name())
+                .email("shiprayadav@gmail.com")
+                .phoneNo("7776688888").build();
+        
+        Patient patient2 = Patient.builder()
+                .name("Avyaan Yadav")
+                .birthDate(LocalDate.of(2021, 12 ,20))
+                .gender(Gender.Male.toString())
+                .email("avyaanyadav@gmail.com")
+                .phoneNo("7776688888").build();
+
+        List<Patient> result = patientRepository.saveAll(List.of(patient, patient2));
         System.out.printf("Patient Saved: %s", result);
         Assert.notNull(patient, "Not null");
     }
@@ -39,8 +66,17 @@ public class PatientTest {
 //        Pageable pageable = Pageable.ofSize(5);
 //        Page<Patient> result2 = repository.findAll(pageable);
 
-        List<Patient> result = repository.findAll();
-        System.out.printf("No. of Patient : %s", result.size());
+        List<Patient> result = patientRepository.findAll();
+        System.out.printf("No. of Patient : %d\n", result.size());
         result.forEach(System.out::println);
+    }
+
+    @Test
+    public void testFindAllPatientId() {
+        Long patientId = 8L;
+        Patient patient = patientService.getPatientById(patientId);
+
+        System.out.printf("Patient with ID: %d Output: %s", patientId, patient);
+
     }
 }
