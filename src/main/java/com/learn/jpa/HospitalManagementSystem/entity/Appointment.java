293c1eb8314082d56a6e1785ea63b47899310d1f
Deepak Yadav <deepakyadav1331@gmail.com>
2025-09-04 21:50:46 +0530

    Added Appointment as ManyToOne relationship with Patient

---
+++ src/main/java/com/learn/jpa/HospitalManagementSystem/entity/Appointment.java
@@ -0,0 +1,37 @@
+package com.learn.jpa.HospitalManagementSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 128)
+    private String reason;
+
+    @Column(nullable = false)
+    private LocalDateTime appointmentTime;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdOn;
+
+    //Owning Side, since appointment doesn't exist without patient,
+    //here appointment owns the relationship
+    @ManyToOne
+    @JoinColumn(name = "patient_id", nullable = false) //patient is required and non nullable
+    private Patient patient;
+}
