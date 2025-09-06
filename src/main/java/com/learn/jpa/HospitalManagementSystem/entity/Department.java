6d8c942171b6281fe46858986ba20322f1e22ca3
Deepak Yadav <deepakyadav1331@gmail.com>
2025-09-06 18:50:46 +0530

    Added Department

---
+++ src/main/java/com/learn/jpa/HospitalManagementSystem/entity/Department.java
@@ -0,0 +1,34 @@
+package com.learn.jpa.HospitalManagementSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String name;
+
+    @OneToOne
+    private Doctor headDoctor;
+
+    @ManyToMany
+    @JoinTable(
+            name = "my_dept_doctors",
+            joinColumns = @JoinColumn(name = "dept_id"),
+            inverseJoinColumns = @JoinColumn(name = "doctor_id")
+    )
+    private Set<Doctor> doctors = new HashSet<>();
+}
