63843c950996adb0b05d00d9a779c7633e00958c
Deepak Yadav <deepakyadav1331@gmail.com>
2025-09-19 14:27:21 +0530

    Added InMemoryUserDetailsManager

--- src/main/java/com/learn/jpa/HospitalManagementSystem/config/SecurityConfig.java
+++ src/main/java/com/learn/jpa/HospitalManagementSystem/config/SecurityConfig.java
@@ -1,16 +1,24 @@
 package com.learn.jpa.HospitalManagementSystem.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.io.IOException;
-
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
 
     @Bean
@@ -26,10 +34,18 @@ public class SecurityConfig {
     }
 
 
-    /*@Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        //Following code will make override the default (all endpoints as authenticated,
-        // to all as public api now, although default password are still getting generated)
-        return httpSecurity.formLogin(Customizer.withDefaults()).build();
-    }*/
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user1 = User.withUsername("admin").password(passwordEncoder.encode("password"))
+                            .roles("ADMIN").build();
+
+        UserDetails user2 = User.withUsername("patient").password(passwordEncoder.encode("password"))
+                .roles("ADMIN").build();
+
+        UserDetails user3 = User.withUsername("doctor").password(passwordEncoder.encode("password"))
+                .roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user1, user2, user3);
+    }
 }
