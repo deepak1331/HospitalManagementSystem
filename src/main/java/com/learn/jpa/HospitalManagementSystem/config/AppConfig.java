a2ebe706b2949f6a269026647d669fabfddfcc7d
Deepak Yadav <deepakyadav1331@gmail.com>
2025-09-23 20:41:11 +0530

    Added CustomUserDetailsService, SecurityConfig

--- src/main/java/com/learn/jpa/HospitalManagementSystem/config/AppConfig.java
+++ src/main/java/com/learn/jpa/HospitalManagementSystem/config/AppConfig.java
@@ -3,8 +3,12 @@ package com.learn.jpa.HospitalManagementSystem.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class AppConfig {
@@ -18,4 +22,20 @@ public class AppConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+
+    /*@Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user1 = User.withUsername("admin").password(passwordEncoder().encode("password"))
+                .roles("ADMIN").build();
+
+        UserDetails user2 = User.withUsername("patient").password(passwordEncoder().encode("password"))
+                .roles("PATIENT").build();
+
+        UserDetails user3 = User.withUsername("doctor").password(passwordEncoder().encode("password"))
+                .roles("DOCTOR").build();
+
+        return new InMemoryUserDetailsManager(user1, user2, user3);
+    }*/
 }
---
+++ src/main/java/com/learn/jpa/HospitalManagementSystem/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.learn.jpa.HospitalManagementSystem.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/doctors/**").hasAnyRole("ADMIN", "DOCTOR"))
+                .formLogin(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+
+}
